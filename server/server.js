daeabdedf5bc092cec7ac754aae4f95615638854
affan <affanalam2019>
2024-06-10 18:59:20 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const pdf = require("html-pdf");
-const path = require("path"); 
+const path = require("path");
 const pdfSample1 = require("./Templates/template-1");
 const pdfSample2 = require("./Templates/template-2");
 const pdfSample3 = require("./Templates/template-3");
 const phantomjs = require("phantomjs-prebuilt");
 
 const app = express();
-const port =process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -33,13 +33,15 @@ app.post("/create-pdf", (req, res) => {
       return res.status(400).send("Invalid template name");
   }
 
-  const options = {
-    format: 'Legal',
-    orientation: 'portrait',
-    phantomPath: '/usr/local/bin/phantomjs'
+  var options = {
+    format: "A4",
+    phantomPath:
+      "C:\\Users\\HP\\Downloads\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe",
+    type: "pdf",
+    timeout: 30000,
   };
 
-  const pdfFilePath = path.join(__dirname, "Resume.pdf"); // Generate PDF file path
+  const pdfFilePath = path.join(__dirname, "Resume.pdf");
 
   try {
     pdf.create(selectedTemplate(data), options).toFile(pdfFilePath, (err) => {
@@ -48,7 +50,7 @@ app.post("/create-pdf", (req, res) => {
         return res.status(500).send("Failed to create PDF");
       }
       console.log("PDF created successfully");
-      res.sendFile(pdfFilePath); // Send generated PDF file
+      res.sendFile(pdfFilePath);
     });
   } catch (error) {
     console.error("Unhandled error:", error);
