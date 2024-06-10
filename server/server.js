26dcc9c38e6a569f84176f01571325349e58ed79
affan <affanalam2019>
2024-06-10 16:23:44 +0530

    Fix

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const pdf = require("html-pdf");
+const path = require("path"); // Import path module
 const pdfSample1 = require("./Templates/template-1");
 const pdfSample2 = require("./Templates/template-2");
 const pdfSample3 = require("./Templates/template-3");
+const phantomjs = require("phantomjs-prebuilt"); // Import phantomjs-prebuilt
 
 const app = express();
-const port = 4000;
+const port =process.env.PORT || 4000;
 
-app.use(cors());  // Use CORS middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -31,14 +33,20 @@ app.post("/create-pdf", (req, res) => {
       return res.status(400).send("Invalid template name");
   }
 
+  const options = {
+    phantomPath: phantomjs.path // Specify the path to PhantomJS
+  };
+
+  const pdfFilePath = path.join(__dirname, "Resume.pdf"); // Generate PDF file path
+
   try {
-    pdf.create(selectedTemplate(data), {}).toFile("Resume.pdf", (err) => {
+    pdf.create(selectedTemplate(data), options).toFile(pdfFilePath, (err) => {
       if (err) {
         console.error("PDF creation error:", err);
         return res.status(500).send("Failed to create PDF");
       }
       console.log("PDF created successfully");
-      res.send("<script>alert('PDF created successfully');</script>");
+      res.sendFile(pdfFilePath); // Send generated PDF file
     });
   } catch (error) {
     console.error("Unhandled error:", error);
@@ -49,7 +57,6 @@ app.post("/create-pdf", (req, res) => {
 app.get("/fetch-pdf", (req, res) => {
   res.sendFile(`${__dirname}/Resume.pdf`);
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port=${port}`);
