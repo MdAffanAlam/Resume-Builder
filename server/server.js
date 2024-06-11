68d0119a5e205e868a381ca05b7709ee48fb00ba
affan <affanalam2019>
2024-06-11 15:42:49 +0530

    done

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ const pdfSample3 = require("./Templates/template-3");
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors()); 
+app.use(cors({
+  origin:['http://127.0.0.1:5173/details'],
+  credentials:'true'
+})); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
